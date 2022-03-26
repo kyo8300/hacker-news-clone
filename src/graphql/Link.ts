4b36d94288c5b259ed81cb10fbf382f15747ec22
Fujita Kyotaka <kametyou777@gmail.com>
2022-03-26 18:54:59 +0800

    🚀 add authentication

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -7,6 +7,18 @@ export const Link = objectType({
     t.nonNull.int("id");
     t.nonNull.string("description");
     t.nonNull.string("url");
+    t.field("postedBy", {
+      type: "User",
+      resolve(parent, args, context) {
+        return context.prisma.link
+          .findUnique({
+            where: {
+              id: parent.id,
+            },
+          })
+          .postedBy();
+      },
+    });
   },
 });
 
@@ -33,11 +45,17 @@ export const LinkMutation = extendType({
       },
       resolve(parent, args, context) {
         const { description, url } = args;
+        const { userId } = context;
+
+        if (!userId) {
+          throw new Error("Cannot post without logging in.");
+        }
 
         const newLink = context.prisma.link.create({
           data: {
             description,
             url,
+            postedBy: { connect: { id: userId } },
           },
         });
 
