a5db296cdde68ab77bee527e93614b730afcd8ef
Fujita Kyotaka <kametyou777@gmail.com>
2022-03-27 20:40:09 +0800

    add custom scalar

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -7,6 +7,7 @@ export const Link = objectType({
     t.nonNull.int("id");
     t.nonNull.string("description");
     t.nonNull.string("url");
+    t.nonNull.dateTime("createdAt");
     t.field("postedBy", {
       type: "User",
       resolve(parent, args, context) {
@@ -17,6 +18,14 @@ export const Link = objectType({
             },
           })
           .postedBy();
+      },
+    });
+    t.nonNull.list.nonNull.field("voters", {
+      type: "User",
+      resolve(parent, args, context) {
+        return context.prisma.link
+          .findUnique({ where: { id: parent.id } })
+          .voters();
       },
     });
   },
